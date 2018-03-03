c699e04364c52999c87c7e971b1c1079f07e1068
mariojankovic <majankovi@gmail.com>
2018-03-03 15:12:10 +0100

    Fixed html and deploy folder

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,9 @@ module.exports = {
     filename: 'build.js'
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
   ],
   module: {
     rules: [
